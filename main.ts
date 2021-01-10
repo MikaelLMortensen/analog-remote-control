3c4eb60aa2c4ed653fe05c59afb6a24b6e94c01a
MikaelLMortensen <MikaelLMortensen@users.noreply.github.com>
2021-01-10 12:35:38 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -8,29 +8,36 @@ let speedStruct = { lf : true, rf:true, ls:0, rs:0 }
 radio.setGroup(20)
 
 input.onButtonPressed(Button.AB, function () {
-    speedStruct.lf = true
-    speedStruct.rf = true
-    sendSpeedInfo()
+    radio.sendValue("lf", 1)
+    radio.sendValue("rf", 1)
+    showSpeedInfo()
 })
 
 input.onButtonPressed(Button.A, function () {
     speedStruct.lf = !speedStruct.lf
-    sendSpeedInfo()
+    if (speedStruct.lf) {
+        radio.sendValue("lf", 1)
+    } else {
+        radio.sendValue("lf", 0)
+    }
+    showSpeedInfo()
 })
 
 input.onButtonPressed(Button.B, function () {
     speedStruct.rf = !speedStruct.rf
-    sendSpeedInfo()
+    if (speedStruct.rf) {
+        radio.sendValue("rf", 1)
+    } else {
+        radio.sendValue("rf", 0)
+    }
+    showSpeedInfo()
 })
 
-function sendSpeedInfo() {
-    let json = JSON.stringify(speedStruct)
-    radio.sendString(json)
-    showSpeedInfo()
-}
-
 function showSpeedInfo(){
-
+    if (speedStruct.ls < 5 && speedStruct.rs < 5) {
+        basic.clearScreen()
+        return
+    }
     if (speedStruct.lf && speedStruct.rf) {
         basic.plotLeds(`
         . # . # .
@@ -67,19 +74,17 @@ function showSpeedInfo(){
 }
 
 basic.forever(function () {
-    let changed = false
     let oldVoltageP1 = 0
     let oldVoltageP2 = 0
 
 	let voltageP1 = pins.analogReadPin(AnalogPin.P1)
     let voltageP2 = pins.analogReadPin(AnalogPin.P2)
     if (voltageP1 != oldVoltageP1) {
-        changed = true
-    } else if (voltageP2 != oldVoltageP2) {
-        changed = true
-    }
-
-    if (changed) {
-        sendSpeedInfo()        
+        radio.sendValue("ls", voltageP1)
+        oldVoltageP1 = voltageP1
+    } 
+    if (voltageP2 != oldVoltageP2) {
+        radio.sendValue("rs", voltageP2)
+        oldVoltageP2 = voltageP2
     }
 })
